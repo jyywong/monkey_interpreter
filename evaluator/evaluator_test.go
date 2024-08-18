4dcc9fd4a9595fc69c7cb43fa12ff39224b3c131
jyywong <wong.jonathan1@gmail.com>
2024-08-18 14:37:32 -0400

    Added let statement eval

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -38,7 +38,8 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
-	return Eval(program)
+	env := object.NewEnvironment()
+	return Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
@@ -196,6 +197,10 @@ if (10 > 1) {
 }
 `,
 			"unknown operator: BOOLEAN + BOOLEAN",
+		},
+		{
+			"foobar",
+			"identifier not found: foobar",
 		},
 	}
 
--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"monkey_interpreter/evaluator"
 	"monkey_interpreter/lexer"
+	"monkey_interpreter/object"
 	"monkey_interpreter/parser"
 )
 
@@ -13,7 +14,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
+	env := object.NewEnvironment()
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -28,7 +29,7 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
